e12c138c0a16d87e4ff631d0cd357bbd75283941
sameer <sameersmail@gmail.com>
2013-07-10 16:47:54 +0000

    web.js edited for index.hml

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
-
+var fs=require('fs');
 var app = express.createServer(express.logger());
 
+
+fs.readFile('index.html',function(err,indexBuf){
+    if(err){console.log('cannot read index.html');}
+};
+
+	    
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    
+  response.send(indexBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
